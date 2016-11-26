6664efea5c75dcf0eae3e1423ba9d451ea3086b0
Viswadeep Sarangi <deepun.deep@gmail.com>
2016-11-26 17:53:25 +0530

    added audio, finished painting the whole mangalyaan. first page menu text looks weird. also, there is too much light on the orbiter

---
+++ Assets/Scripts/functions_to_be_called.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class functions_to_be_called : MonoBehaviour {
+	GameObject sound_button_gameobject;
+	Text t;
+	// Use this for initialization
+	void Start () {
+		sound_button_gameobject = GameObject.Find ("Canvas/sound_button");
+		t = sound_button_gameobject.GetComponentInChildren<Text> ();
+		PlayerPrefs.SetInt ("sound_enable", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void load_scene(string level_name){
+		SceneManager.LoadScene (level_name);
+	}
+
+	public void toggle_sound(){
+		if (PlayerPrefs.GetInt ("sound_enable", 1) == 1) {
+			PlayerPrefs.SetInt ("sound_enable", 0);
+			t.text = "SOUND : OFF";
+			Debug.Log ("Sound is now Disabled");
+		} else {
+			PlayerPrefs.SetInt ("sound_enable", 1);
+			t.text = "SOUND : ON";
+			Debug.Log ("Sound is now Enabled");
+		}
+	}
+
+	public void exit_app(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/image_target_script.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class image_target_script : MonoBehaviour,ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	GameObject mangalyaan_super_container_GO;
+	GameObject audio_mangalyaan;
+
+	// Update is called once per frame
+	void Update () {}
+
+	void Start()
+	{
+		mangalyaan_super_container_GO = GameObject.Find ("mangalyaan_super_container");
+		set_renderers_of_all_children_disabled_for_mangalyaan_super_container_GO ();
+
+		audio_mangalyaan = GameObject.Find ("audio_mangalyaan_doc");
+		//audio_mangalyaan.GetComponent<AudioSource> ().Stop ();
+
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+
+	}
+
+	void set_renderers_of_all_children_enabled_for_mangalyaan_super_container_GO(){
+		Renderer[] component_array= mangalyaan_super_container_GO.GetComponentsInChildren<Renderer> ();
+		for (int i = 0; i < component_array.Length; i++) {
+			component_array [i].enabled = true;
+		}
+		//mangalyaan_super_container_GO.GetComponent<Renderer> ().enabled = false;
+	}
+	void set_renderers_of_all_children_disabled_for_mangalyaan_super_container_GO(){
+		Renderer[] component_array= mangalyaan_super_container_GO.GetComponentsInChildren<Renderer> ();
+		for (int i = 0; i < component_array.Length; i++) {
+			component_array [i].enabled = false;
+		}
+		//mangalyaan_super_container_GO.GetComponent<Renderer> ().enabled = false;
+	}
+
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			if (previousStatus == TrackableBehaviour.Status.NOT_FOUND ||
+			    previousStatus == TrackableBehaviour.Status.UNDEFINED ||
+			    previousStatus == TrackableBehaviour.Status.UNKNOWN) 
+			{				
+				if (PlayerPrefs.GetInt ("sound_enable") == 1) {
+					audio_mangalyaan.GetComponent<AudioSource> ().Play ();
+				}
+				set_renderers_of_all_children_enabled_for_mangalyaan_super_container_GO ();
+
+			}
+			// Play audio when target is found
+			//audio.Play();
+		}
+		else
+		{
+			if (previousStatus == TrackableBehaviour.Status.DETECTED ||
+			    previousStatus == TrackableBehaviour.Status.TRACKED ||
+			    previousStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) 
+			{
+				set_renderers_of_all_children_disabled_for_mangalyaan_super_container_GO ();
+				audio_mangalyaan.GetComponent<AudioSource> ().Stop ();
+			}
+		}
+	}   
+}
